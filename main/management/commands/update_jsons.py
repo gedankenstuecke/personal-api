1be28db7de97b387f4976250e8755a5ece1d304a
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-10-19 15:05:05 -0700

    heroku cheat

--- main/management/commands/update_jsons.py
+++ main/management/commands/update_jsons.py
@@ -1,12 +1,15 @@
 from django.core.management.base import BaseCommand
 from openhumans.models import OpenHumansMember
 from main import helpers
+import requests
 
 
 class Command(BaseCommand):
     help = 'Process so far unprocessed data sets'
 
     def handle(self, *args, **options):
+        #how to keep the heroku app alive despite a free dyno :joy:
+        requests.get('http://my-personal-api.herokuapp.com/')
         oh_members = OpenHumansMember.objects.all()
         for oh_member in oh_members:
             helpers.compile_music(oh_member)
